c776de2c31bf0846b50d64ddb413905c790fe8de
Selu349 <57500649+Selu349@users.noreply.github.com>
2020-06-30 23:17:03 +0200

    Add files via upload

---
+++ Project Bug/Scripts/Bullet.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+# Varaiables for moveing and deleting
+var move_direction = Vector2(0,0)
+export(int) var speed = 10
+export(float) var live_time = 5 # in seconds
+# Damage
+export(int) var dmg = 10
+
+func _ready():
+	$Timer.start(live_time)
+
+func _physics_process(delta):
+	if move_direction != Vector2(0,0):
+		global_position += move_direction * speed
+
+func _on_Timer_timeout():
+	destroy_bullet()
+
+func _on_Area2D_area_entered(area):
+	if area.is_in_group("PlayerHitbox"):
+		area.get_parent().take_damage(dmg)
+		destroy_bullet()
+	if area.is_in_group("SwordHitbox"):
+		destroy_bullet()
+
+func destroy_bullet():
+	queue_free()
---
+++ Project Bug/Scripts/Enemys/Emy 02 States.gd
@@ -0,0 +1,84 @@
+extends StateMachine
+
+var prev_state
+#onready var center = get_parent().get_node("Center")
+
+func _ready():
+	add_state("idle")
+	add_state("walk")
+	add_state("attack") # Attacking the (a) player
+	add_state("shoot")
+	add_state("retreat") # if the player is too close, try to go away
+	call_deferred("set_state", states.idle)
+
+func state_logic(_delta):
+	match state:
+		states.idle:
+			if prev_state != states.idle:
+				parent.get_node("time_between_walk").set_paused(false)
+				parent.get_node("time_between_walk").start()
+		states.walk:
+			parent.apply_velocity()
+			parent.turning()
+		states.attack:
+			parent.apply_velocity()
+			if prev_state != states.attack:
+				parent.get_node("time_between_walk").set_paused(true)
+				parent.get_node("time_between_walk").stop()
+			parent.attack()
+			parent.turning()
+		states.shoot:
+			if parent.player_node != null:
+				if parent.is_shooting == true:
+					parent.shoot_bullets()
+					parent.is_shooting = false
+				if parent.get_node("Time_between_shots").is_paused() == true:
+					parent.get_node("Time_between_shots").set_paused(false)
+		states.retreat:
+			parent.apply_velocity()
+			parent.retreat()
+			parent.turning()
+	prev_state = state
+
+
+func get_transition(_delta):
+	match state:
+		states.idle:
+			if parent.velocity != Vector2(0,0) && !parent.is_attacking:
+				return states.walk
+			elif parent.is_attacking:
+				return states.attack
+		states.walk:
+			if parent.velocity == Vector2(0,0):
+				return states.idle
+			elif parent.is_attacking:
+				return states.attack
+		states.attack:
+			if !parent.is_attacking:
+				return states.idle
+			if parent.player_node != null:
+				if parent.player_node.global_position.distance_to(parent.global_position) <= parent.max_shoot_distance:
+					return states.shoot
+		states.shoot:
+			if parent.player_node != null:
+				if parent.player_node.global_position.distance_to(parent.global_position) > parent.max_shoot_distance && parent.is_attacking:
+					return states.attack
+				elif parent.player_node.global_position.distance_to(parent.global_position) < parent.min_shoot_distance:
+					return states.retreat
+			else:
+				return states.idle
+		states.retreat:
+			if parent.player_node != null:
+				if parent.player_node.global_position.distance_to(parent.global_position) > parent.min_shoot_distance:
+					return states.attack
+
+func enter_state(_new_state, _old_state):
+	match _new_state:
+		states.idle:
+			parent.get_node("Sprite/AnimationPlayer").play("idle")
+		states.walk:
+			parent.get_node("Sprite/AnimationPlayer").play("walk")
+		# There is no attack animation yet !
+		#---------------------------------
+		states.attack:
+			parent.get_node("Sprite/AnimationPlayer").play("walk")
