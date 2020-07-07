1ee4ee20812bcc926182732a38b872c166c06f50
TeoTheO <57500649+TeoTheOO@users.noreply.github.com>
2020-07-07 16:51:28 +0200

    Add files via upload

---
+++ Project Bug BACKUP 15/Scripts/Enemys/Emy 02 Range.gd
@@ -0,0 +1,151 @@
+extends KinematicBody2D
+
+
+# Basic Movement Varaiables
+var velocity = Vector2()
+var move_direction = Vector2()
+export(int) var max_speed = 280
+export(float) var acceleration = 140
+export(float) onready var friction = max_speed / 2
+export(float) var attack_speed_multiplyer = 1.25
+var x_rand = [1, -1]
+var x_dir = -1
+var prev_x_dir = x_dir
+# Timer Varaiables
+export(float) var walk_time = 1.5
+var walk_timer = walk_time
+var nums = [1,0,-1] #list to choose from
+# Check varaiables (Mainly for states)
+var is_attacking = false
+var is_walking = false
+# For attacking the player (node)
+var player_node
+# Varaiables for combat
+# ---------------------
+# ---------------------
+export(float) var max_health = 50
+var health = max_health
+export(int) var damage = 5 # The damage it can make (to players)
+# Varaiables For Shooting
+export(int) var shoot_angle = 360  # in degrees
+export(int) var numb_of_bullets = 10
+export(float) var time_between_shots = 0.5
+export(int) var radius_x = 100 # For bullet spawning
+var radius = Vector2(radius_x, 0)
+export(int) var max_shoot_distance = 400
+export(int) var min_shoot_distance = 150
+var bullet = preload("res://Scenes/Bullet.tscn")
+var is_shooting = true
+onready var center = get_node("Center")
+# ---------------------
+# ---------------------
+
+func _ready():
+	randomize()
+	$time_between_walk.set_wait_time(rand_range(2, 2.8))
+	$walk_time.set_wait_time(rand_range(0.8,1.4))
+	$Time_between_shots.set_wait_time(time_between_shots)
+	$Time_between_shots.start()
+	$Time_between_shots.set_paused(true)
+	shoot_angle = deg2rad(shoot_angle)
+	x_dir = x_rand[randi() % x_rand.size()]
+
+func apply_velocity():
+	velocity = move_and_slide(velocity)
+# For walking
+func choose_random_dir():
+	return nums[randi() % nums.size()]
+
+
+# Walking (in  arandom direction)
+#--------------------------------
+func time_between_walk_timeout(delta):
+	get_node("time_between_walk").set_paused(true)
+	move_direction.x = choose_random_dir()
+	x_dir = move_direction.x
+	move_direction.y = choose_random_dir()
+	walk_timer = get_node("walk_time").get_wait_time()
+#	is_walking = true
+	walk()
+	get_node("walk_time").start()
+
+func walk():
+	while walk_timer > 0:
+		velocity = velocity.move_toward(move_direction.normalized() * max_speed, acceleration)
+		walk_timer = get_node("walk_time").get_time_left()
+#	if walk_timer <= 0:
+#		is_walking = false
+
+func walk_time_timeout():
+	while velocity != Vector2(0,0):
+		velocity = velocity.move_toward(Vector2(0,0), friction)
+	get_node("time_between_walk").set_paused(false)
+# --------------------------------
+
+
+# Attack (the player)
+#-----------------------------------------
+func AttackRange_area_entered(area):
+	if area.is_in_group("PlayerHitbox"):
+		is_attacking = true
+		player_node = area.get_parent()
+
+func AttackRange_area_exited(area):
+	if area.is_in_group("PlayerHitbox"):
+		is_attacking = false
+		player_node = null
+		while velocity != Vector2(0,0):
+			velocity = velocity.move_toward(Vector2(0,0), friction)
+
+func attack(delta):
+	if player_node != null:
+		move_direction = self.get_global_position().direction_to(player_node.get_global_position()).normalized()
+		x_dir = move_direction.round().x # For turning
+		velocity = velocity.move_toward(move_direction * max_speed * attack_speed_multiplyer, acceleration * delta)
+	else:
+		is_attacking = false
+		$Time_between_shots.set_paused(true)
+
+func _on_Time_between_shots_timeout():
+	is_shooting = true
+
+func shoot_bullets():
+	var angle_step
+	if numb_of_bullets > 1:
+		angle_step = shoot_angle / (numb_of_bullets - 1)
+	else:
+		angle_step = shoot_angle
+#	center.rotation = center.global_position.direction_to(player_node.global_position).angle()
+	center.look_at(player_node.global_position)
+	
+	for i in numb_of_bullets:
+#		var spawn_pos = center.global_position + radius.rotated((center.rotation / 2) + angle_step * i) # * sign(center.global_rotation / 2))
+		
+		var spawn_pos = center.global_position + radius.rotated(center.rotation + -shoot_angle / numb_of_bullets * 2.0 + angle_step * i)
+		
+		var node = bullet.instance()
+		node.global_position = spawn_pos
+		get_parent().add_child(node)
+		node.move_direction = global_position.direction_to(node.global_position)
+
+func retreat(delta):
+	if player_node != null:
+		move_direction = player_node.global_position.direction_to(self.global_position).normalized()
+		x_dir = move_direction.round().x # for turning
+		velocity = velocity.move_toward(move_direction * max_speed * attack_speed_multiplyer, acceleration * delta)
+
+func turning():
+	if x_dir != 0 && x_dir != prev_x_dir:
+		prev_x_dir = x_dir
+		scale.x *= -1
+
+func take_damage(dmg):
+	health -= dmg
+	if health <= 0:
+		death()
+
+func death():
+	queue_free()
+
+
+
---
+++ Project Bug BACKUP 15/Scripts/HandlingWeapon/WeaponHandle.gd
@@ -0,0 +1,110 @@
+extends KinematicBody2D
+
+# Damage
+export(int) var damage = 6
+export(int) onready var max_damage = damage * 5
+export(float) var damage_multiplyer = 0.02
+var enemy_nodes = []
+export(int) var area_damage = 2
+# Where to move varaiables
+# ------------------------
+onready var center = get_parent()
+var move_dir = Vector2(0,0)
+var move_to_pos = Vector2(0,0)
+export(int) var max_distance_point = 25 # Maximum distance from move_to_pos (shouldn't be too big)
+# ------------------------
+var velocity = Vector2(0,0)
+export(int) var max_speed = 7400
+export(int) onready var friction = max_speed / 2
+export(int) var acceleration = 1600
+var pos = Vector2(0,0)
+# Rotation varaiables
+# -------------------
+export(float) var smooth_spd = 20
+export(float) var rotate_speed = 25
+export(int) var center_dis = 40 # In pixels
+export(float) var max_rand_rotation = 0.1
+var rand_rotation
+# Varaibles for states
+# --------------------
+var is_moveing = true
+var has_landed = false
+onready var weapon_states = get_node("WeaponStates")
+
+
+func apply_velocity():
+	velocity = move_and_slide(velocity)
+#	print(get_local_mouse_position().distance_to(center.position))
+
+func rotate_toward_mouse(delta):
+	global_rotation += get_local_mouse_position().angle() * smooth_spd * delta
+func rotate_around_center():
+	global_position = get_target()
+
+# Setting up the position it'll be moveing toward 
+# and sets "has_landed" for states 
+func setup_throw():
+	move_to_pos = get_global_mouse_position()
+	move_dir = (get_global_mouse_position() - global_position).normalized()
+	pos = global_position
+	has_landed = false
+
+
+func rotate_when_thrown(delta):
+	global_rotation += rotate_speed * delta
+func move_to_throw_pos(delta):
+	# Move if not at the cursor's remembered position
+	# Else stop
+	if global_position.distance_to(move_to_pos) >= max_distance_point:
+		pos += move_dir * acceleration * delta
+	else:
+		velocity = velocity.move_toward(Vector2(0,0), friction)
+		if velocity == Vector2(0,0):
+			has_landed = true
+	
+	global_position = pos
+	rotate_when_thrown(delta)
+
+
+# stay in position when LANDED
+func setup_stay_at_pos():
+	move_to_pos = global_position
+func stay_at_pos():
+	global_position = move_to_pos
+
+
+func just_landed_effect():
+	# particles, no damage
+	pass
+func landed_effect():
+	# particles, if enemy enters an area then damage
+	pass
+
+func rotate_when_back_to_center(delta):
+	global_rotation -= rotate_speed * delta
+func back_to_center(delta):
+	if global_position.distance_to(get_target()) >= max_distance_point / 2:
+		move_dir = (get_target() - global_position).normalized()
+		pos += move_dir * acceleration * delta
+		rotate_when_back_to_center(delta)
+	else:
+		# If i don't teleport it to the target, it might go back and forth 
+		# towards the point, because it takes too big steps, 
+		# but it's not visible for humans in my opinion
+		pos = get_target()
+	
+	global_position = pos
+
+func get_target(): # The target is the position wich I want to have it in the idle state
+	var direction : Vector2 = get_global_mouse_position() - center.global_position
+	direction = direction.normalized() * center_dis
+	return center.global_position + direction
+
+# Handling Damage dealt by the sword itself
+# -----------------------------------------
+func SwordHitbox_area_entered(area):
+	if area.is_in_group("EnemyHitbox") && (weapon_states.state == weapon_states.states.moveing || weapon_states.state == weapon_states.states.call_back):
+		if $SwordSpeed.speed != 0:
+			var dmg = min(damage + round(damage_multiplyer * $SwordSpeed.speed), max_damage)
+			area.get_parent().take_damage(dmg)
+
